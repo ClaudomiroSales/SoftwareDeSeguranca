ab0732624e3a6351c473bc831994b8ecb644c0ab
Claudomiro Sales <cssj@ufpa.br>
2019-07-03 01:49:28 -0300

    Faltavam fazer alguns includes e foi adicionado exemplo typeid
    #include <vector>
    using std::vector;
    
    #include "SoftwareDeSeguranca.h"
    #include "Firewall.h"
    #include "Antispyware.h"
    #include "Antivirus.h"
    #include "McAfee.h"
    #include "Panda.h"
    
    
    	vector< SoftwareDeSeguranca * > seguranca;
        seguranca.push_back(new Firewall("192.168.0.2", "255.255.255.0") );
        seguranca.push_back(new Firewall("168.192.0.1", "255.255.255.0") );
        seguranca.push_back(new Antispyware("McAfee", "Antivirus") );
        seguranca.push_back(new Antispyware( "Panda", "Antivirus") );
        
        
        
        for( size_t i = 0; i < seguranca.size( ); i++ )
    	{
    		cout << "deleting object of "
    		<< typeid( *seguranca[ i ] ).name() << '\n';
    		delete seguranca[ i ];
    	}

--- main.cpp
+++ main.cpp
@@ -6,6 +6,10 @@ using std::vector;
 
 #include "SoftwareDeSeguranca.h"
 #include "Firewall.h"
+#include "Antispyware.h"
+#include "Antivirus.h"
+#include "McAfee.h"
+#include "Panda.h"
 
 using std::cout;
 using std::cin;
@@ -14,16 +18,20 @@ using std::string;
 int main(int argc, char **argv)
 {
  
-	Firewall test( "192.168.0.2", "255.255.255.0" );
-    vector< SoftwareDeSeguranca * > seguranca;
-    //seguranca.push_back(new Firewall("192.168.0.2", "255.255.255.0") );
-    //seguranca.push_back(new Firewall("168.192.0.1", "255.255.255.0") );
-    //seguranca.push_back(new Antispyware("McAfee", "Antivirus") );
-    //seguranca.push_back(new Antispyware( "Panda", "Antivirus") );
+	vector< SoftwareDeSeguranca * > seguranca;
+    seguranca.push_back(new Firewall("192.168.0.2", "255.255.255.0") );
+    seguranca.push_back(new Firewall("168.192.0.1", "255.255.255.0") );
+    seguranca.push_back(new Antispyware("McAfee", "Antivirus") );
+    seguranca.push_back(new Antispyware( "Panda", "Antivirus") );
     
     
     
-    
+    for( size_t i = 0; i < seguranca.size( ); i++ )
+	{
+		cout << "deleting object of "
+		<< typeid( *seguranca[ i ] ).name() << '\n';
+		delete seguranca[ i ];
+	}
     
     
 }
